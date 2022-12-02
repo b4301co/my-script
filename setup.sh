c62de5649ffbeac490c02a9bb7d435521d779f83
b4391co <b4391co@proton.me>
2022-12-03 00:46:08 +0100

    zsh div / title

--- setup.sh
+++ setup.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
-
-mkdir ~/.app_def
-mkdir ~/.app_def/zsh-plugins
-
-
-
+echo " ____  _  _  _____ ___  _  ____ ___    ____  _____ _____ _   _ ____"
+echo "| __ )| || ||___ // _ \/ |/ ___/ _ \  / ___|| ____|_   _| | | |  _ \ "
+echo "|  _ \| || |_ |_ \ (_) | | |  | | | | \___ \|  _|   | | | | | | |_) |"
+echo "| |_) |__   _|__) \__, | | |__| |_| |  ___) | |___  | | | |_| |  __/"
+echo "|____/   |_||____/  /_/|_|\____\___/  |____/|_____| |_|  \___/|_|"
+echo ""
+echo ""
 echo "[ + ] ( 1 ) - Cambiar Host"
 echo "[ + ] ( 2 ) - Cambiar IP"
 echo "[ + ] ( 3 ) - Cambiar resolv"
 echo "[ + ] ( 4 ) - Instalar apts"
-echo "[ + ] ( F ) - Fonts ( ROOT )"
-echo "[ + ] ( 5 ) - Instalar zsh + zshrc + div" 
-echo "[ + ] ( 6 ) - Instalar nvim-code"
-echo "[ + ] ( 7 ) - Instalar hamachi" 
-#echo "[ + ] ( 6 ) - Alias" 
-#echo "[ + ] ( 7 ) - Instalar term div ejecutar desde la ruta" 
+echo "[ + ] ( 5 ) - Alacritty"
+echo "[ + ] ( 6 ) - Instalar ZSH/OMZsh" 
+echo "[ + ] ( 7 ) - Instalar nvim-code"
+echo "[ + ] ( 0 ) - Exit"
 
 read var_opcion
 
@@ -33,9 +32,9 @@ then
 	echo "nueva IP ( def = 192.168.1.X ): "
 	read new_address
     	echo "nueva mascara (def = 255.255.255.0 ): "
-   	read new_netmask
+	read new_netmask
     	echo "nuevo gateway ( def = 192.168.1.1 ): "
- 	read new_gateway
+	read new_gateway
     	echo "nueva interfaz (def = eth0 ): "
     	read new_interface
     	echo "
@@ -102,9 +101,12 @@ then
 	wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
 	dpkg -i lsd_*
 	rm lsd_*
+fi
+if [ $var_opcion = 5 ]
+then
 	#Alacritty
 	sudo cp alacritty.yml /usr/share
-	 cp alacritty.yml ~/.alacritty.yml
+	cp alacritty.yml ~/.alacritty.yml
 	apt-get install cmake -y
 	apt-get install pkg-config -y
 	apt-get install libfreetype6-dev -y
@@ -121,85 +123,135 @@ then
 	sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
 	sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 	sudo desktop-file-install extra/linux/Alacritty.desktop
-	sudo update-desktop-database
-	
-	
-fi
+	sudo update-desktop-database	
 
-if [ $var_opcion = 'F' ]
-then
+	# Fonts
 	cd /usr/local/share/fonts
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
 	unzip Hack.zip
 	rm Hack.zip
+
+	#echo
+	echo "source $HOME/.cargo/env"
+	echo "rustup override set stable"
+	echo "cargo build --release"
 fi
 
-if [ $var_opcion = 5 ]
-then
-	#ZSH
-	apt install zsh -y
-	
-	# OHMYZSH + FONTS & STYLES 
-	apt install curl -y
-	apt install wget -y
-	apt install fonts-powerline -y
-	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -s
-	git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-	
-	#COPYS
-	cp p10k.zsh.2 ~/.p10k.zsh
-	cp zshrc ~/.zshrc
-	cp zellij ~/.app_def/zellij
-	cp config.yaml ~/.app_def/config.yaml
-	cp nvim.conf ~/.app_def/nvim.conf
-	cp web-search.plugin.zsh ~/.app_def/zsh-plugins
-	chmod a+x ~/.app_def/zellij
-
-	#PLUGIN
-	cd ~/.app_def/zsh-plugins
-
-	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-	git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-
-	wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
-	
-	cd ~ 
-	source ~/.zshrc	
-fi
 if [ $var_opcion = 6 ]
+then
+	echo ""
+	echo ""
+	echo "[ + ] ( 1 ) - All"
+	echo "[ + ] ( 2 ) - ZSH"
+	echo "[ + ] ( 3 ) - OH-MY-ZSH"
+	echo "[ + ] ( 4 ) - Plugins"
+	echo "[ + ] ( 5 ) - Clear"
+
+	read var_opcion6
+
+	mkdir ~/.app_def
+	mkdir ~/.app_def/zsh-plugins
+
+	if [ $var_opcion6 = 1 ]
+	then
+
+		#ZSH
+		apt install zsh -y
+		
+		# OHMYZSH + FONTS & STYLES 
+		apt install curl -y
+		apt install wget -y
+		apt install fonts-powerline -y
+		sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -s
+		git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+		
+		#COPYS
+		cp p10k.zsh.2 ~/.p10k.zsh
+		cp zshrc ~/.zshrc
+		cp zellij ~/.app_def/zellij
+		cp config.yaml ~/.app_def/config.yaml
+		cp nvim.conf ~/.app_def/nvim.conf
+		cp web-search.plugin.zsh ~/.app_def/zsh-plugins
+		chmod a+x ~/.app_def/zellij
+
+		#PLUGIN
+		cd ~/.app_def/zsh-plugins
+
+		git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+		git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+		wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
+		
+		cd ~ 
+		source ~/.zshrc	
+	fi
+	
+	if [ $var_opcion6 = 2 ]
+	then
+		#ZSH
+		apt install zsh -y
+		cd ~ 
+		source ~/.zshrc	
+	fi
+
+	if [ $var_opcion6 = 3 ]
+	then
+		# OHMYZSH + FONTS & STYLES 
+		apt install curl -y
+		apt install wget -y
+		apt install fonts-powerline -y
+		sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -s
+		git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+		#COPYS
+		cp p10k.zsh.2 ~/.p10k.zsh
+		cp zshrc ~/.zshrc
+		cp zellij ~/.app_def/zellij
+		cp config.yaml ~/.app_def/config.yaml
+		cp nvim.conf ~/.app_def/nvim.conf
+		cp web-search.plugin.zsh ~/.app_def/zsh-plugins
+		chmod a+x ~/.app_def/zellij
+		cd ~ 
+		source ~/.zshrc	
+	fi	
+	
+	if [ $var_opcion6 = 4 ]
+	then
+		#PLUGIN
+		cd ~/.app_def/zsh-plugins
+
+		git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+		git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+		wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
+		cd ~ 
+		source ~/.zshrc	
+	fi
+
+	if [ $var_opcion6 = 5 ]
+	then
+		cd ~ 
+		rm .zshrc
+		rm .p10k.zsh
+		rm -rfv .oh-my-zsh/
+		rm -rfv .zsh/	
+	fi
+fi
+if [ $var_opcion = 7 ]
 then
 	echo ''
 	echo 'bash <(curl -s https://gitlab.com/claudiobrt/nvim-code/-/raw/master/utils/install.sh)'
 	echo ''
 	unalias nvim
 fi
-if [ $var_opcion = 7 ]
+if [ $var_opcion = 0 ]
 then
-	wget https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb
-	dpkg -i logmein-*
-	rm logmein-*
+	echo exit
 fi
-
-
-
-#if [ $var_opcion = 6 ]
-#then
-	## ADDs 
-	#echo alias "z='zsh'" >> ~/.bashrc
-	#echo alias "ll='ls -l'" >> ~/.zshrc
-	#echo alias "lh='ls -lh' | more" >> ~/.zshrc
-	#echo alias "nv='nvim -u ~/.app_def/nvim.conf'" >> ~/.zshrc
-	#source ~/.zshrc
-	#echo " z = zsh // ll = ls -l // lh = ls -lh // v = nvim "
-#fi
-
-#if [ $var_opcion = 7 ]
-#then
-	#cp zellij ~/app_def/zellij
-	#cp config.yaml ~/app_def/config.yaml
-	#chmod a+x ~/app_def/zellij
-	#echo alias "div='~/app_def/zellij -c ~/app_def/config.yaml options --disable-mouse-mode'" >> ~/.zshrc
-	#zsh
-#fi
-
+echo " _____ ___ _   _ ___ ____  _   _ "
+echo "|  ___|_ _| \ | |_ _/ ___|| | | |"
+echo "| |_   | ||  \| || |\___ \| |_| |"
+echo "|  _|  | || |\  || | ___) |  _  |"
+echo "|_|   |___|_| \_|___|____/|_| |_|"
