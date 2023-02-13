b6989d871e8e7b02567b3dcd882cb8a1b9ecd54c
b4391co <b4391co@proton.me>
2023-02-13 02:53:22 +0100

    target rojo

--- polybar/scripts/target_to_hack.sh
+++ polybar/scripts/target_to_hack.sh
@@ -2,7 +2,7 @@ ip_address=$(cat /home/b4391co/.config/polybar/scripts/target | awk '{print $1}'
 machine_name=$(cat /home/b4391co/.config/polybar/scripts/target | awk '{print $2}')
  
 if [ $ip_address ] && [ $machine_name ]; then
-    echo "%{F#ffffff}${F#ffffff}$ip_address%{u-} - $machine_name"
+    echo "%{F#ff0000}${F#ff0000}$ip_address%{u-} - $machine_name"
 else
-    echo "${F#ffffff}%{u-}%{F#ffffff} No target"
+    echo "${F#ff0000}%{u-}%{F#ff0000} No target"
 fi
